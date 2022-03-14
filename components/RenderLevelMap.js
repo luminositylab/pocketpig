ac5765881f3a21268428b07800514a331f42481b
Callum Song <callumsong@Callums-MacBook-Air.local>
2022-03-14 14:21:27 -0700

    LevelScreen

---
+++ components/RenderLevelMap.js
@@ -0,0 +1,59 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import LevelMap from './../assets/LevelMap.svg'
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+
+export default function RenderLevelMap() {
+  return (
+    <View style={styles.container}>
+      <LevelMap style={{
+        position: 'absolute',
+        top: hp("15%"),
+        left: wp("12%"),
+        right: 0,
+        bottom: 0,
+      }}/>
+      <TouchableOpacity style={{
+        width: wp("27%"), 
+        height: hp("11%"), 
+        position: 'absolute',
+        top: hp("27%"),
+        left: wp("47%"),
+        right: 0,
+        bottom: 0,
+      }}
+      onPress={() => {console.log('button1')}}/>
+      <TouchableOpacity style={{
+        width: wp("27%"), 
+        height: hp("11%"), 
+        position: 'absolute',
+        top: hp("48%"),
+        left: wp("20%"),
+        right: 0,
+        bottom: 0,
+    }}
+    onPress={() => {console.log('button2')}}/>
+      <TouchableOpacity style={{
+        width: wp("27%"), 
+        height: hp("11%"), 
+        position: 'absolute',
+        top: hp("68%"),
+        left: wp("12%"),
+        right: 0,
+        bottom: 0,
+    }}
+    onPress={() => {console.log('button3')}}/>
+      {/*<Text>Pocket Pig is ready for development! :)</Text>*/}
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
