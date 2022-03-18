23c61af407285cf63aa8c718d200af0b711f92cf
Apurv Thakur <athaku24@sundevils.asu.edu>
2022-03-18 12:08:07 -0700

    adding home component

--- App.js
+++ App.js
@@ -1,24 +1,23 @@
-import React from 'react'
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import RenderLevelMap from './components/RenderLevelMap'
-import Level1 from './components/Level1'
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import RenderLevelMap from "./components/RenderLevelMap";
+import Level1 from "./components/Level1";
+import Home from "./components/Home";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  
-
   return (
-    
     <View style={styles.container}>
       {/* <Text>Pocket Pig is ready for development! :)</Text> */}
       <NavigationContainer>
         <Stack.Navigator initialRouteName="RenderLevelMap">
           <Stack.Screen name="RenderLevelMap" component={RenderLevelMap} />
           <Stack.Screen name="Level1" component={Level1} />
+          <Stack.Screen name="Home" component={Home} />
         </Stack.Navigator>
       </NavigationContainer>
       <StatusBar style="auto" />
@@ -29,7 +28,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    marginTop: 0
+    backgroundColor: "#fff",
+    marginTop: 0,
   },
 });
