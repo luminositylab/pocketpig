b4b0cfc57fa7f6a2a5586a5f9f4a3a363d10ed45
Callum Song <callumsong@Callums-MacBook-Air.local>
2022-04-18 10:28:29 -0700

    fixed potential conflicts with merge

--- App.js
+++ App.js
@@ -5,24 +5,23 @@ import { NavigationContainer } from "@react-navigation/native";
 import RenderLevelMap from "./components/RenderLevelMap";
 import Level1 from "./components/Level1";
 import Home from "./components/Home";
-import CollectingCoins from './components/Level1Game/CollectingCoins';
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import CollectingCoins from "./components/Level1Game/CollectingCoins";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <CollectingCoins />
-      {/* <Text>Pocket Pig is ready for development! :)</Text> 
+      {/* <Text>Pocket Pig is ready for development! :)</Text> */}
       <NavigationContainer>
         <Stack.Navigator initialRouteName="Home">
           <Stack.Screen name="Home" component={Home} />          
           <Stack.Screen name="RenderLevelMap" component={RenderLevelMap} />
           <Stack.Screen name="Level1" component={Level1} />
+          <Stack.Screen name="CollectingCoins" component={CollectingCoins} />
         </Stack.Navigator>
       </NavigationContainer>
-      */}
       <StatusBar style="auto" />
     </View>
   );
